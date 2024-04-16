214a1b60640d885f8d5d3f8476a53e5432d71176
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-16 10:42:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+let title = 'random title';
+const handleClick = () => {
+title = 'hello people'
+console.log(title);
+
+};
+
+return(
+<React.Fragment>
+<h2>{title}</h2>
+<button type='button' className='btn' onClick={handleClick}>
+change title 
+</button>
+</React.Fragment>
+
+)
 }
-
 export default App;
