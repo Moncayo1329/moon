aceaae39304d0b16d0914250cd5e6eb86d127416
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-16 11:14:37 +0100

    4hours

--- src/usestate.js
+++ src/usestate.js
@@ -5,9 +5,25 @@ function UseState()
 {
 
 const [text,setText] = useState('random title');
+
+const handleClick = () => {
+if( text === 'random title'){
+
+setText('hello world') ;
+}
+else{
+    setText('random title')
+   
+
+};
+}
+
 return (
 <React.Fragment><h1>{text}</h1>
+<button className="btn" onClick={handleClick}>
+change title
 
+</button>
 
 
 
