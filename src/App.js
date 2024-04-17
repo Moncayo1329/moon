d9b9e9a81568819a518edd9123ffe796f8ad0341
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-17 11:10:29 +0100

    4:31

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import UseState from './usestate';
 import Array from './array'
 import Objectexample from './Objectexample'
+import UseStateCounter from './useStateCounter';
 
 
 
@@ -12,6 +13,7 @@ return(
 <UseState />
 <Array />
 <Objectexample />
+<UseStateCounter />
 
 
 </React.Fragment>
